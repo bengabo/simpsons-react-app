d9035551543fdfafbb3cab7a66f2755d89bf94f4
Benjamin Gaboury <ben@bengabo.com>
2019-10-08 11:33:05 +0200

    2nd commit: navbar

--- react/simpsons-app/src/App.js
+++ react/simpsons-app/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Navbar from './Navbar/Navbar'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <Navbar />
       </header>
     </div>
   );
---
+++ react/simpsons-app/src/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <nav>
+        <img title="Homer Simpson" alt="Homer Simpson" src="https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png"/>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
